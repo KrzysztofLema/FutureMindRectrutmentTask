662c178aef298c3655b75880fea9119b401da148
Krzysztof Lema <krzysztof.lema@intive.com>
2022-04-08 00:29:57 +0200

    Add Table View to the view
    **User story**:
    
    As a designer i would like to present table view on the screen
    
    **Task number**:
    
    7
    
    **Description**
    
    Add table view to the list View
    
    **Design:**
    N/A
    
    **Tests:**
    iPhone 13 Pro Max
    
    Build and run and check if the table view is appearing with simple text

--- FutureMind/FutureMind/UI/List/ListView.swift
+++ FutureMind/FutureMind/UI/List/ListView.swift
@@ -11,12 +11,45 @@ class ListView: UIView {
 
     let viewModel: ListViewModel
 
+    let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.insetsContentViewsToSafeArea = true
+        tableView.contentInsetAdjustmentBehavior = .automatic
+        return tableView
+    }()
+
+    private func setupView() {
+        addSubview(tableView)
+        tableView.dataSource = self
+    }
+
+    override func layoutSubviews() {
+      super.layoutSubviews()
+      tableView.frame = bounds
+    }
+
     init(frame: CGRect = .zero, viewModel: ListViewModel) {
         self.viewModel = viewModel
         super.init(frame: frame)
+        setupView()
     }
 
     required init?(coder: NSCoder) {
         fatalError("View is created without Nib files")
     }
+
+}
+extension ListView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = "HEllo"
+        return cell!
+    }
+
+
 }
