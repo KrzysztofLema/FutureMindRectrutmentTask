3b52fc03f44695ce34bc8f6593827ae5eff5d733
Krzysztof Lema <krzysztof.lema@intive.com>
2022-04-07 20:23:33 +0200

    Add Xcode Project Template
    **User story**:
    N/A
    
    **Task number**:
    1
    
    **Description**
    
    Added Xcode project template with Unit Test and UITest targets.
    
    **Design:**
    N/A
    
    **Tests:**
    iPhone Pro Max (15.2)
    Application is running
    
    Run Unit and UI Tests

---
+++ FutureMind/FutureMind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FutureMind
+//
+//  Created by Krzysztof Lema on 07/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
